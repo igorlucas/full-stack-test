23f92209e76352211518b91e0efab4ba6fc0be64
Igor Lucas <igorlucas.silva@bol.com.br>
2019-08-28 11:40:29 -0300

    criando banco de dados json etratando  background de dua imagenss

--- main.js
+++ main.js
@@ -1,6 +1,163 @@
-// console.debug('Hello, Javascript!');
-function validarForm() {
-    console.log('clicou');
-    var form = document.getElementsByTagName(form);
-    console.log(form);
-}
+window.onload = function () {
+
+    let msg = document.getElementById("msg");
+    let erroNome = document.querySelector('#erroNome');
+    let erroEmail = document.querySelector('#erroEmail');
+    let erroMensagem = document.querySelector('#erroMensagem');
+    let formulario = document.getElementById('form');
+
+    enviarForm = (e) => {
+        validarForm(formulario);
+    }
+
+    validarForm = (formulario) => {
+        var a = validarNome(formulario.nome);
+        var b = validarEmail(formulario.email);
+        var c = validarMensagem(formulario.mensagem);
+
+        if (a && b && c) post({ nome: formulario.nome.value, email: formulario.email.value, mensagem: formulario.mensagem.value });
+    }
+
+    function validarMensagem(data) {
+        let mensagem = data.value;
+        var valid = true;
+
+        erroMensagem.style.display = 'none';
+        data.className = "";
+
+        if (!mensagem) {
+            erroMensagem.value = "o campo mensagem é obrigatório";
+            valid = false;
+        } else if (mensagem.trim().split(" ").length < 4) {
+            erroMensagem.value = "deve ter no mínimo 4 palavras";
+            valid = false;
+        } else if (mensagem.trim().length < 20) {
+            erroMensagem.value = "deve ter no mínimo 20 caracteres";
+            valid = false;
+        }
+        if (!valid) {
+            data.className = "error";
+            erroMensagem.style.display = "block";
+            data.focus();
+        }
+        return valid;
+    }
+
+    function validarEmail(data) {
+
+        let email = data.value;
+        var valid = true;
+
+        erroEmail.style.display = 'none';
+        data.className = "";
+
+        var dominio = email.substring(email.indexOf("@") + 1, email.length);
+
+        if (!email) {
+            console.log('o campo email é obrigatório');
+            erroEmail.value = "o campo email é obrigatório";
+            valid = false;
+        } else if (email.length < 3 || email.indexOf("@") <= 0 || dominio.length < 1) {
+            console.log('campo email inválido');
+            erroEmail.value = "campo email inválido";
+            valid = false;
+        }
+        if (!valid) {
+            data.className = "error";
+            erroEmail.style.display = "block";
+            data.focus();
+        }
+        return valid;
+    }
+
+    function validarNome(data) {
+        let nome = data.value;
+        var valid = true;
+        erroNome.style.display = 'none';
+        data.className = "";
+
+        if (!nome) {
+            console.log('o campo nome é obrigatório');
+            erroNome.value = "o campo nome é obrigatório";
+            valid = false;
+        }
+        else if (nome.trim().length < 7) {
+            console.log('o campo nome deve ter no mínimo 7 caracteres');
+            erroNome.value = "o campo nome deve ter no mínimo 7 caracteres";
+            valid = false;
+        } else if (nome.trim().split(' ').length < 2) {
+            console.log('o campo nome deve ter no minímo 2 palavras');
+            erroNome.value = "o campo nome deve ter no minímo 2 palavras";
+            valid = false;
+        }
+
+        if (!valid) {
+            data.className = "error";
+            erroNome.style.display = "block";
+            data.focus();
+        }
+        return valid;
+    };
+
+    function post(data) {
+        const BASE_URL = '/contact'
+
+        var ajax = new XMLHttpRequest();
+        ajax.open("POST", `${BASE_URL}`, true);
+        ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        ajax.send(`nome=${data.nome}&email=${data.email}&mensagem=${data.mensagem}`);
+
+        ajax.onreadystatechange = function () {
+            erroNome.style.display = 'none';
+            formulario.nome.className = "";
+            erroEmail.style.display = 'none';
+            formulario.email.className = "";
+            erroMensagem.style.display = 'none';
+            formulario.mensagem.className = "";
+
+
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                var data = ajax.responseText;
+                console.log(data);
+                msg.style.display = "block"
+                setTimeout(() => {
+                    msg.style.display = "none"
+                }, 3000);
+
+                formulario.reset();
+            } else if (ajax.readyState == 4 && ajax.status == 422) {
+
+                let result = ajax.responseText;
+                const { errors } = JSON.parse(result);
+
+                errors.forEach((e) => {
+                    switch (e.campo) {
+                        case 'nome': {
+                            erroNome.style.display = 'block';
+                            erroNome.value = e.erros[0];
+                            formulario.nome.className = "error";
+                            formulario.nome.focus();
+                            break;
+                        }
+                        case 'email': {
+                            erroEmail.style.display = 'block';
+                            erroEmail.value = e.erros[0];
+                            formulario.email.className = "error";
+                            formulario.email.focus();
+                            break;
+                        }
+                        case 'mensagem': {
+                            erroMensagem.style.display = 'block';
+                            erroMensagem.value = e.erros[0];
+                            formulario.mensagem.className = "error";
+                            formulario.mensagem.focus();
+                            break;
+                        }
+                    }
+                });
+
+            }
+        }
+    }
+};
--- server.js
+++ server.js
@@ -1,1 +1,107 @@
-const ParcelBundler = require('parcel-bundler')const express = require('express')const app = express()const index = 'index.html'const port = 8000const bundler = new ParcelBundler(index, {watch: true})app.use(express.urlencoded({ extended: true }))/** * Contact form data validation and persistence must be made in this route! */app.post('/contact', (req, res) => {  return res.status(200).send(`Contact form data: ${JSON.stringify(req.body)}`)})app.use(bundler.middleware())app.listen(port, () => console.log(`Server running at http://localhost:${port}/`))
+const ParcelBundler = require('parcel-bundler')
+const express = require('express')
+const app = express()
+
+const index = 'index.html'
+const port = 8000
+const bundler = new ParcelBundler(index, { watch: true })
+
+app.use(express.urlencoded({ extended: true }))
+
+/**
+ * Contact form data validation and persistence must be made in this route!
+ */
+app.post('/contact', (req, res) => {
+
+  const listaErros = [];
+
+  const { nome, email, mensagem } = req.body;
+
+  if (validarNome(nome).erros)
+    listaErros.push(validarNome(nome))
+
+  if (validarEmail(email).erros)
+    listaErros.push(validarEmail(email))
+
+  if (validarMensagem(mensagem).erros)
+    listaErros.push(validarMensagem(mensagem))
+
+  if (listaErros.length > 0) {
+    return res.status(422).json({ errors: listaErros });
+  }
+  else {
+    post({ nome, email, mensagem });
+    return res.status(200).send(`Contact form data: ${JSON.stringify(req.body)}`)
+  }
+
+})
+
+validarNome = (nome) => {
+  const erros = [];
+
+  if (!nome)
+    erros.push("o campo nome é obrigatório");
+  if (nome.trim().length < 7)
+    erros.push("deve ter no mínimo 7 caracteres");
+  if (nome.trim().split(' ').length < 2)
+    erros.push('deve ter no minímo duas palavras');
+
+  if (erros.length > 0)
+    return { campo: 'nome', erros: erros };
+  else
+    return { erros: null }
+};
+
+validarEmail = (email) => {
+  const erros = [];
+
+  var dominio = email.substring(email.indexOf("@") + 1, email.length);
+
+  if (!email)
+    erros.push('o campo email é obrigatório');
+
+  if (email.length < 3 || email.indexOf("@") <= 0 || dominio.length < 1)
+    erros.push('campo email inválido');
+
+  if (erros.length > 0)
+    return { campo: 'email', erros: erros };
+  else
+    return { erros: null }
+
+}
+
+validarMensagem = (mensagem) => {
+
+  const erros = [];
+
+  if (!mensagem)
+    erros.push("o campo mensagem é obrigatório");
+
+  if (mensagem.trim().split(" ").length < 4)
+    erros.push("deve ter no mínimo 4 palavras");
+
+  if (mensagem.trim().length < 20)
+    erros.push("deve ter no mínimo 20 caracteres");
+
+  if (erros.length > 0)
+    return { campo: 'mensagem', erros: erros };
+  else
+    return { erros: null }
+}
+
+post = (data) => {
+  const BASE_URL = 'http://localhost:3000/posts'
+  var request = require('request');
+  request.post(
+    BASE_URL,
+    { json: data },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    }
+  );
+}
+
+app.use(bundler.middleware())
+app.listen(port, () => console.log(`Server running at http://localhost:${port}/`))
